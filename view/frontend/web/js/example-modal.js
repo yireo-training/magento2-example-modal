9b8b15ef3364ce0936212822de18c10855265cd1
Jisse Reitsma <jisse@yireo.com>
2017-05-28 12:37:58 +0200

    Pending changes

---
+++ view/frontend/web/js/example-modal.js
@@ -0,0 +1,18 @@
+define([
+        "jquery", "Magento_Ui/js/modal/modal"
+    ], function($){
+        ExampleModal = {
+            initModal: function(config, element) {
+                $target = $(config.target);
+                $target.modal();
+                $element = $(element);
+                $element.click(function() {
+                    $target.modal('openModal');
+                });
+            }
+        };
+        return {
+            example-modal: ExampleModal.initModal
+        };
+    }
+);
